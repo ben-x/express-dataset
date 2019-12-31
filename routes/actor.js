223065558000727fead53aff1688aea4a404a7fb
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-12-31 16:04:38 +0100

    Update actor.js

--- routes/actor.js
+++ routes/actor.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {updateActor, getAllActors, getStreak} = require('../controllers/actors');
+const controller = require('../controllers/actors');
 
 // Routes related to actor.
-router.put('/', updateActor);
-router.get('/', getAllActors);
-router.get('/streak', getStreak);
+router.put('/', controller.updateActor);
+router.get('/', controller.getAllActors);
+router.get('/streak', controller.getStreak);
 
 module.exports = router;
