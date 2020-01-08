0ea9439c9bcacb5a78f660f6ca4ebf3cfa8dbba3
Michael Owolabi <michael.i.owolabi@gmail.com>
2020-01-08 12:18:31 +0100

    fix foreign key contraint issue

--- db/events.js
+++ db/events.js
@@ -1,5 +1,6 @@
 const db = require('./index');
 
+db.exec("PRAGMA foreign_keys = ON")
 db.serialize(() => {
   db.run("DROP TABLE IF EXISTS events")
   db.run("CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, actor_id, repo_id, created_at datetime default current_timestamp, FOREIGN KEY (actor_id) REFERENCES actors(id), FOREIGN KEY (repo_id) REFERENCES repos(id))", (err) => {
