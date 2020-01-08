0f4aea115fececd4c74a7de24b958ab566dc5422
Michael Owolabi <michael.i.owolabi@gmail.com>
2020-01-08 08:46:09 +0100

    implement update for actors endpoint

--- controllers/actors.js
+++ controllers/actors.js
@@ -20,8 +20,63 @@ var getAllActors = (req, res) => {
 	})
 };
 
-var updateActor = () => {
+// Update actor's avatar url
+var updateActor = (req, res) => {
+	const { id, login, avatar_url } = req.body;
+	let updateError = false;
+	const findActor = `SELECT * FROM actors WHERE id = ?`;
+	const updateAvatar = `UPDATE actors SET avatar_url = ? WHERE id = ?`;
 
+	const params = [avatar_url, id]
+
+	db.serialize(() => {
+
+		db.get(findActor,[params[1]], (err, rows) => {
+			if (err) {
+				updateError = true;
+				return res.status(500).send({
+					error: true,
+					message: 'Network error, please try again later'
+				});
+			}
+			
+			// Check if actor exist
+			if (rows === undefined) {
+				updateError = true;
+				return res.status(404).send({
+					error: true,
+					message: 'User not found',
+				});
+			}
+
+			// Check if other fields are being updated by the user
+			if (login !== rows.login) {
+				updateError = true;
+				return res.status(400).send({
+					error: true,
+					message: 'You can only update your avatar url',
+				});
+			}
+		})
+		
+		// Update the avatar url
+		db.run(updateAvatar, params, (err) => {
+			if (updateError) {
+				return
+			}
+			if (err) {
+				console.log(err)
+				return res.status(500).send({
+					error: true,
+					message: 'Network error, please try again later'
+				});
+			}
+			return res.status(200).send({
+				error: false,
+				message: 'Profile successfully updated',
+			});
+		})
+	})
 };
 
 var getStreak = () => {
--- routes/actor.js
+++ routes/actor.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { getAllActors } = require('../controllers/actors');
+const { getAllActors, updateActor } = require('../controllers/actors');
 
 // Routes related to actor.
 router.get('/actors', getAllActors);
-
+router.put('/actors', updateActor);
 
 module.exports = router;
