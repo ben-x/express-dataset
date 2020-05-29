d8a824905b1d00eeeef65c19a70ecc3d020f695e
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-29 11:25:00 +0100

    add controller to get actors by streak

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,16 +1,23 @@
 const db = require('../database/db-config');
+const { getActors } = require('../utils/getActors');
 
+/**
+ * Get all actors controller
+ *
+ * @param {request} req
+ * @param {response} res
+ */
 const getAllActors = (req, res) => {
 	let result;
 
 	db.find({})
-		.sort({ 'actor.id': -1, created_at: -1, 'actor.login': -1 })
+		.sort({ actor: -1, created_at: -1, 'actor.login': 1 })
 		.exec((err, docs) => {
 			if (err) {
 				res.status(500).end();
 			}
 
-			result = docs.map((doc) => doc.actor);
+			result = [...new Set(getActors(docs))];
 
 			res.status(200).json({
 				result,
@@ -18,6 +25,12 @@ const getAllActors = (req, res) => {
 		});
 };
 
+/**
+ * Updates an actor avatar url
+ *
+ * @param {request} req
+ * @param {response} res
+ */
 const updateActor = (req, res) => {
 	if (Object.entries(req.body).length > 3) {
 		res.status(400).end();
@@ -44,7 +57,23 @@ const updateActor = (req, res) => {
 	}
 };
 
-const getStreak = () => {};
+const getStreak = (req, res) => {
+	let actors;
+
+	db.find({})
+		.sort({ created_at: -1 })
+		.exec((err, docs) => {
+			if (err) {
+				throw new Error(err);
+			}
+
+			actors = [...new Set(getActors(docs))];
+
+			res.status(200).json({
+				actors,
+			});
+		});
+};
 
 module.exports = {
 	updateActor: updateActor,
