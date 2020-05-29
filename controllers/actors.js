770c9075c53ae6abd652678c4f1f91078f28d8ae
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-29 02:02:46 +0100

    add controller to update an actor

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,36 +1,37 @@
+const db = require('../database/db-config');
 
-var getAllActors = () => {
-	
+const getAllActors = () => {};
+
+const updateActor = (req, res) => {
+	if (Object.entries(req.body).length > 3) {
+		res.status(400).end();
+	} else {
+		const { id, avatar_url } = req.body;
+
+		db.findOne({ 'actor.id': id }, (err, doc) => {
+			if (err) {
+				throw new Error(err);
+			}
+
+			if (doc === null) {
+				res.status(404).json({
+					message: 'Actor not found',
+				});
+			} else {
+				doc.actor.avatar_url = avatar_url;
+
+				res.status(200).json({
+					doc: doc.actor,
+				});
+			}
+		});
+	}
 };
 
-var updateActor = () => {
-
-};
-
-var getStreak = () => {
-
-};
-
+const getStreak = () => {};
 
 module.exports = {
 	updateActor: updateActor,
 	getAllActors: getAllActors,
-	getStreak: getStreak
+	getStreak: getStreak,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
