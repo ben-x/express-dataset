23cd0fe8fdb738dc283b1c7b8dfdc6279fa4578c
tosinibrahim96 <tosinibrahim96@gmail.com>
2020-07-01 09:58:49 +0100

    Chore: complete express dataset challenge
    - Erasing all the events
    - Adding new events
    - Returning all the events
    - Returning the event records filtered by the actor ID
    - Updating the avatar URL of the actor
    - Returning the actor records ordered by the total number of events
    - Returning the actor records ordered by the maximum streak

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,36 +1,76 @@
+const db = require("../models");
 
-var getAllActors = () => {
-	
+const getAllActors = async (req, res, next) => {
+  try {
+    const sql = `SELECT a.id, a.login, a.avatar_url FROM actors a INNER JOIN events e ON (e.actor_id = a.id) GROUP BY a.id ORDER BY COUNT() DESC, e.created_at DESC, a.login ASC`;
+
+    const result = await db.getAll(sql);
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-var updateActor = () => {
+const updateActor = async (req, res, next) => {
+  try {
+    const body = req.body;
 
+    if (Object.keys(body).length > 3) {
+      return res.status(400).json({});
+    }
+
+    if (!body.id || !body.avatar_url) {
+      return res.status(400).json({});
+    }
+
+    const actor = await db.getOne(`SELECT * FROM actors WHERE id = ?`, [
+      body.id,
+    ]);
+    if (!actor) {
+      return res.status(404).json({});
+    }
+
+    if (body.login !== actor.login || body.id !== actor.id) {
+      return res.status(400).json({});
+    }
+
+    const result = await db.execute(
+      `UPDATE actors SET avatar_url = ? WHERE id = ?`,
+      [body.avatar_url, body.id]
+    );
+
+    res.status(200).json({});
+  } catch (error) {
+    next(error);
+  }
 };
 
-var getStreak = () => {
+const getStreak = async (req, res, next) => {
+  try {
+    const sql = `
+                SELECT a.id, a.login, a.avatar_url,COUNT(*) as streak FROM
 
+                (SELECT e1.actor_id,e1.created_at,date(e1.created_at,-(SELECT COUNT(*) FROM events e2 WHERE julianday(e1.created_at)-julianday(e2.created_at))||' day')AS grp from events e1) events
+                INNER JOIN actors a ON (actor_id = a.id)
+                GROUP BY actor_id
+                ORDER BY streak DESC, created_at DESC, a.login ASC`;
+
+    const result = await db.getAll(sql);
+
+    const r = result.map((res)=>{
+      delete res.streak;
+      return res;
+    })
+
+    res.status(200).json(r);
+  } catch (error) {
+    next(error);
+  }
 };
-
 
 module.exports = {
-	updateActor: updateActor,
-	getAllActors: getAllActors,
-	getStreak: getStreak
+  updateActor,
+  getAllActors,
+  getStreak,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- controllers/events.js
+++ controllers/events.js
@@ -1,42 +1,120 @@
+const db = require("../models");
+const formatEventOutput = require("../helpers/formatEventOutput");
 
-var getAllEvents = () => {
+const getAllEvents = async (req, res, next) => {
+  try {
+    const query = `SELECT *, e.id FROM events e INNER JOIN actors a ON a.id = e.actor_id INNER JOIN repos r ON r.id = e.repo_id ORDER BY e.id ASC`;
+    const result = await db.getAll(query);
+    const events = formatEventOutput(result);
 
+    res.status(200).json(events);
+  } catch (error) {
+    next(error);
+  }
 };
 
-var addEvent = () => {
+const addEvent = async (req, res, next) => {
+  try {
+    const body = req.body;
 
+    if (
+      !body.id ||
+      !body.type ||
+      !body.actor ||
+      !body.repo ||
+      !body.created_at
+    ) {
+      return res
+        .status(400)
+        .json({ error: "content missing. Please verify your data" });
+    }
+
+    const result = await db.getOne(`SELECT * FROM events WHERE id = ?`, [
+      body.id,
+    ]);
+
+    if (result) {
+      return res.status(400).json({});
+    }
+
+    const oneActor = await db.getOne(`SELECT * FROM actors WHERE id = ?`, [
+      body.actor.id,
+    ]);
+    if (!oneActor) {
+      const { id: actor_id, login, avatar_url } = body.actor;
+      await db.execute(
+        `INSERT INTO actors (id, login, avatar_url)
+      VALUES (?, ?, ?)`,
+        [actor_id, login, avatar_url]
+      );
+    }
+
+    const oneRepo = await db.getOne(`SELECT * FROM repos WHERE id = ?`, [
+      body.repo.id,
+    ]);
+    if (!oneRepo) {
+      const { id: repo_id, name, url } = body.repo;
+      await db.execute(
+        `INSERT INTO repos (id, name, url)
+        VALUES (?, ?, ?)`,
+        [repo_id, name, url]
+      );
+    }
+
+    const { id, type, created_at, actor, repo } = body;
+    await db.execute(
+      `INSERT INTO events (id, type, actor_id, repo_id, created_at)
+        VALUES (?, ?, ?, ?, ?)`,
+      [id, type, actor.id, repo.id, created_at]
+    );
+
+    res.status(201).json({});
+  } catch (error) {
+    next(error);
+  }
 };
 
+const getByActor = async (req, res, next) => {
+  try {
+    const params = req.params;
+    const actor = await db.getOne(`SELECT * FROM actors WHERE id = ?`, [
+      params.actorId,
+    ]);
 
-var getByActor = () => {
+    if (!actor) {
+      return res.status(404).json({});
+    }
 
+    const query = `SELECT e.id, e.type,e.created_at,a.id as actor_id,a.login,a.avatar_url,r.id as repo_id,r.name,r.url
+    FROM events e
+    INNER JOIN actors a ON (a.id= e.actor_id)
+    INNER JOIN repos r ON (r.id =e.repo_id)
+    WHERE e.actor_id = ?
+    ORDER BY e.id ASC;`;
+
+    const result = await db.getAll(query, [params.actorId]);
+    const events = formatEventOutput(result);
+
+    res.status(200).json(events);
+  } catch (error) {
+    next(error);
+  }
 };
 
+const eraseEvents = async (req, res, next) => {
+  try {
+    const query = `DELETE FROM events;`;
 
-var eraseEvents = () => {
-
+    const events = await db.execute(query);
+    res.status(200).json({ events });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-	getAllEvents: getAllEvents,
-	addEvent: addEvent,
-	getByActor: getByActor,
-	eraseEvents: eraseEvents
+  getAllEvents,
+  addEvent,
+  getByActor,
+  eraseEvents,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
