f53707690975ce2e940a1c335d800cf4fc7a247f
courage173 <courageosemwengie@gmail.com>
2020-08-11 22:20:55 +0100

    created crud operations for events

--- controllers/events.js
+++ controllers/events.js
@@ -1,20 +1,70 @@
+var db = require('../models/dbConnection');
 
-var getAllEvents = () => {
-
+var getAllEvents = (req, res) => {
+	try {
+		db.find({})
+			.sort({ _id: 1 })
+			.exec(function (err, docs) {
+				if (err) return res.status(400).json({ success: false, message: err.message })
+				docs.forEach((doc) => { delete doc._id })
+				return res.status(200).json(docs)
+			});
+	} catch (err) {
+		return res.status(401).send(err.message)
+	}
 };
 
-var addEvent = () => {
-
+var addEvent = (req, res) => {
+	try {
+		db.findOne({ id: req.body.id }, (_, doc) => {
+			if (doc != null) {
+				return res.status(400).json({ success: false, message: 'Duplicate' });
+			}
+			db.insert(req.body, (err, doc) => {
+				return res.status(201).json({
+					id: doc.id,
+					type: doc.type,
+					actor: doc.actor,
+					repo: doc.repo,
+					created_at: doc.created_at
+				})
+			});
+		})
+	} catch (err) {
+		return res.status(401).send(err.message)
+	}
 };
 
 
-var getByActor = () => {
+var getByActor = (req, res) => {
+	try {
+		const { actorID } = req.params
+		db.find({ "actor.id": Number(actorID) })
+			.sort({ _id: 1 })
+			.exec(function (err, docs) {
+				if (err) return res.status(400).json({ success: false, message: err.message })
+				if (docs.length < 1) {
+					return res.status(404).json({ success: false, message: 'Event not find' })
+				}
+				docs.forEach((doc) => { delete doc._id })
+				return res.status(200).json(docs)
 
+			})
+	} catch (err) {
+		return res.status(401).send(err.message)
+	}
 };
 
 
-var eraseEvents = () => {
-
+var eraseEvents = (req, res) => {
+	try {
+		db.remove({}, { multi: true }, function (err, numRemoved) {
+			if (err) return res.status(400).json({ success: false, message: err.message })
+			return res.status(200).json({ success: true, message: `deleted ${numRemoved} successfully` })
+		});
+	} catch (err) {
+		return res.status(401).send(err.message)
+	}
 };
 
 module.exports = {
--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var { addEvent } = require('../controllers/events')
+var { addEvent, getAllEvents, getByActor } = require('../controllers/events')
 
 
 // Routes related to event
 router.post("/", addEvent);
+router.get("/", getAllEvents);
+router.get("/actors/:actorID", getByActor);
 
 module.exports = router;
