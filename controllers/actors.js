6ee3c3d554d34a5ea4eb396a90b0a07ed1e66723
chibuike-vincent <chibuikepatrick2@gmail.com>
2021-08-13 12:40:54 +0100

    implemented actors controllers

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,14 +1,36 @@
 
-var getAllActors = () => {
-	
+var db = require("../db_config")
+
+var getAllActors = (req, res) => {
+	db.find({}, { actor: 1, _id: 0 }).sort({ 'actor.login': 1 }).exec((err, result) => {
+		if (err) {
+			throw new Error(err)
+		} else {
+			return res.status(200).send(result)
+		}
+	});
+
 };
 
-var updateActor = () => {
-
+var updateActor = (req, res) => {
+	const data = req.body
+	db.update({ "actor.id": data.id }, { $addToSet: { "actor.avatar_url": data.avatar_url } }, { upsert: true }, (err, numActorsUpdated, updatedActor) => {
+		if (err) throw new Error(err)
+		if (numActorsUpdated === 0) {
+			return res.status(404).send("unable to to update avatar")
+		}
+		return res.status(200).send("avatar updated")
+	})
 };
 
-var getStreak = () => {
-
+var getStreak = (req, res) => {
+	db.find({}, { actor: 1, _id: 0 }).sort({ login: -1 }).exec((err, result) => {
+		if (err) {
+			throw new Error(err)
+		} else {
+			return res.status(200).send(result)
+		}
+	});
 };
 
 
@@ -17,20 +39,3 @@ module.exports = {
 	getAllActors: getAllActors,
 	getStreak: getStreak
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
