b9d2aaf13278525c9fdd2cead7d02a3f3e895faf
chibuike-vincent <chibuikepatrick2@gmail.com>
2021-08-13 12:39:16 +0100

    implemented actors route

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,12 @@
 var express = require('express');
+var controller = require("../controllers/actors")
+
 var router = express.Router();
 
 // Routes related to actor.
+
+router.put("/", controller.updateActor)
+router.get("/", controller.getAllActors)
+router.get("/streak", controller.getStreak)
 
 module.exports = router;
